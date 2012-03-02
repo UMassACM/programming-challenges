212759c038b96e4bbaa4644b769e465b6efef470
Evan Shelhamer <shelhamer@imaginarynumber.net>
2012-03-02 14:05:00 -0500

    Add simple solution to project euler #34
    Compute the sum of all numbers that are equal to the sum of the factorials
    of their digits.
    
    Calculate upper bound and iterate over all candidate numbers.

---
+++ project-euler/project euler p34a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2.7
+
+# Find the sum of all numbers which are equal to
+# the sum of the factorial of their digits.
+# Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
+import math
+from math import factorial, log
+
+# calculate factorial lookup table for 1-9
+factorials = { str(i) : math.factorial(i) for i in range(10) }
+
+# calculate upper bound on numbers with the desired property,
+# based on relation of maximum factorial sums (all 9s) and the number
+# of digits: solve d*9! = 10**d -> d = log(9!)/log(10) + 1
+limit_pow = int(log(factorials['9'])/log(10) + 1)
+limit = 10**limit_pow
+
+sum_curious = 0
+for n in xrange(3, limit): # 1! and 2! excluded since they are not sums
+  factorial_sum = sum([factorials[i] for i in str(n)])
+  if factorial_sum == n:
+    sum_curious += n
+
+print sum_curious
